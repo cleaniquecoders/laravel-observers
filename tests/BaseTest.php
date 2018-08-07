224579a06805996377d4cc69204a3d2209be1677
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-07 14:15:57 +0800

    Added Package Base Test

---
+++ tests/BaseTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace CleaniqueCoders\LaravelObservers\Tests;
+
+class BaseTest extends TestCase
+{
+	/** @test */
+	public function it_has_helpers()
+	{
+		foreach (['abbrv', 'generate_sequence', 'generate_reference', 'hashids'] as $helper) {
+			$this->assertHelperExist($helper);
+		}
+	}
+
+	/** @test */
+	public function it_has_observer_kernel_class()
+	{
+		$this->assertClassExist(\CleaniqueCoders\LaravelObservers\Observers\Kernel::class);
+	}
+
+	/** @test */
+	public function it_has_hashid_observer_class()
+	{
+		$this->assertClassExist(\CleaniqueCoders\LaravelObservers\Observers\HashidsObserver::class);
+	}
+
+	/** @test */
+	public function it_has_hashid_service_class()
+	{
+		$this->assertClassExist(\CleaniqueCoders\LaravelObservers\Services\Hashids::class);
+	}
+
+	/** @test */
+	public function it_has_reference_observer_class()
+	{
+		$this->assertClassExist(\CleaniqueCoders\LaravelObservers\Observers\ReferenceObserver::class);
+	}
+}
