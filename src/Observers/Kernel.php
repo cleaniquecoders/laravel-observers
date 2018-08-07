e50b80777aaf133925b46492ae5f1b3d54193127
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-07 14:00:15 +0800

    Added Observer Kernel, Hashid Observer and Reference Observer

---
+++ src/Observers/Kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CleaniqueCoders\LaravelObservers\Observers;
+
+class Kernel
+{
+    /**
+     * Make this class.
+     *
+     * @return \App\Observers\Kernel
+     */
+    public static function make()
+    {
+        return new self();
+    }
+
+    /**
+     * Register observers.
+     */
+    public function observes()
+    {
+        foreach (config('observers') as $observer => $models) {
+            foreach ($models as $model) {
+                if (class_exists($model) && class_exists($observer)) {
+                    $model::observe($observer);
+                }
+            }
+        }
+    }
+}
---
+++ src/Observers/ReferenceObserver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CleaniqueCoders\LaravelObservers\Observers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * See events.
+ *
+ * @href https://laravel.com/docs/5.5/eloquent#events
+ * Available metthods: retrieved, creating, created, updating, updated,
+ * saving, saved, deleting, deleted, restoring, restored
+ */
+class ReferenceObserver
+{
+    /**
+     * Listen to the creating event.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     */
+    public function creating(Model $model)
+    {
+        $count = method_exists($model, 'getCounter') ? $model->getCounter() : ($model::count() ?? 0);
+
+        $column = method_exists($model, 'getReferenceColumn') ? $model->getReferenceColumn() : 'reference';
+
+        $prefix = method_exists($model, 'referencePrefix') ? $model->referencePrefix() : config('document.prefix');
+
+        if (Schema::hasColumn($model->getTable(), $column) && is_null($model->$column)) {
+            $model->$column = generate_reference($prefix, $count);
+        }
+    }
+}
